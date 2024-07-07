ae26c0ffa96a0876193e244b6f5fc7cf1c191151
RusJJ <mctesting1@yandex.ru>
2024-07-07 13:33:56 +0300

    v1.1

--- main.cpp
+++ main.cpp
@@ -1,39 +1,54 @@
-#include "main.h"
-// a port from pc to android (https://github.com/The-Musaigen/money-separator) 
+// A port from PC to Android (https://github.com/The-Musaigen/money-separator)
+#include <mod/amlmod.h>
+#include <mod/logger.h>
+#include <string>
 
-using namespace std;
+MYMOD(net.KillerSA.MoneySeparator, Money Separator, 1.1, KillerSA)
 
-MYMOD(net.KillerSA.MoneySeparator, Money separator, 1.0, KillerSA)
-
-void (*AsciiToGxt)(const char* txt, unsigned short* ret);
-
-extern "C" void AsciiToGxtChar(const char* aSource, unsigned short* aTarget)
+static std::string AddSeparators(std::string aValue, char aThousandSep = '.') 
 {
-	std::string source = std::string{ aSource };
-	std::string sep = AddSeparators(source);
-    aSource = sep.c_str();
+    int len = aValue.length();
+    int value = (len > 0 && (aValue[0] == '-')) ? 2 : 1;
+    int size = 3;
+
+    while ((len - value) > size)
+    {
+        aValue.insert(len - size, 1, aThousandSep);
+
+        size += 4;
+        len += 1;
+    }
+    return aValue;
+}
+
+DECL_HOOKv(Money_AsciiToGxtChar, const char* aSource, unsigned short* aTarget)
+{
+    std::string source = std::string { aSource };
+    std::string sep = AddSeparators(source);
     
-    AsciiToGxt(aSource, aTarget);
-	return;
+    Money_AsciiToGxtChar(sep.c_str(), aTarget);
 }
 
-uintptr_t ret;
-__attribute__((optnone)) __attribute__((naked)) void Money_Inject(void){
-    asm volatile(
-        "MOV R0, R10\n"
-        "BL AsciiToGxtChar\n"
-        "LDRB R0, [R6]\n");
-    asm volatile("MOV PC, %0" :: "r"(ret));
-}
-
-extern "C" void OnModLoad(){
+extern "C" void OnModLoad()
+{
     logger->SetTag("Money Separator");
     
-    void* hGTASA = aml->GetLibHandle("libGTASA.so");
-    uintptr_t pGTASA = aml->GetLib("libGTASA.so");
-
-    SET_TO(AsciiToGxt, aml->GetSym(hGTASA, "_Z14AsciiToGxtCharPKcPt"));
-    ret = pGTASA + 0x2BD274 + 0x1;
-
-    aml->Redirect(pGTASA + 0x2BD26C, (uintptr_t)Money_Inject);
+    uintptr_t pGame = aml->GetLib("libGTASA.so");
+    if(pGame)
+    {
+        HOOKBLX(Money_AsciiToGxtChar, pGame + BYBIT(0x2BD26E + 0x1, 0x37D4C4));
+    }
+    else
+    {
+        pGame = aml->GetLib("libGTAVC.so");
+        if(pGame)
+        {
+            HOOKBL(Money_AsciiToGxtChar, pGame + BYBIT(0x1E9F74 + 0x1, 0x2C3AC8));
+        }
+        else
+        {
+            logger->Error("This game is unsupported");
+            return;
+        }
+    }
 }
